03d2abd8035bea8ddda0623d866a4393a7707f08
Maxime Houis <maxime.houis@epitech.eu>
2020-04-30 17:08:53 +0200

    feat: add project base

---
+++ MediaPlayer/include/Main.hpp
@@ -0,0 +1,12 @@
+/*
+** MediaPlayer Copyright (C) 2020 Maxime Houis
+** This program comes with ABSOLUTELY NO WARRANTY.
+** This is free software, and you are welcome to redistribute it
+** under certain conditions; see LICENSE for details.
+*/
+
+#pragma once
+
+class Main {
+
+};
---
+++ MediaPlayer/src/Main.cpp
@@ -0,0 +1,16 @@
+/*
+** MediaPlayer Copyright (C) 2020 Maxime Houis
+** This program comes with ABSOLUTELY NO WARRANTY.
+** This is free software, and you are welcome to redistribute it
+** under certain conditions; see LICENSE for details.
+*/
+
+#include <iostream>
+
+#include "Main.hpp"
+
+int main()
+{
+    std::cout << "Hello world!\n";
+    return 0;
+}
