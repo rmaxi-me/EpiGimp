1450d07f0524d89fdb9596f7d77d0c3fc7329d53
Maxime Houis <maxime.houis@epitech.eu>
2020-05-03 12:22:10 +0200

    feat: add Engine base and improve conan

---
+++ Engine/src/Application.cpp
@@ -0,0 +1,8 @@
+/*
+** EpiGimp Copyright (C) 2020 Maxime Houis
+** This program comes with ABSOLUTELY NO WARRANTY.
+** This is free software, and you are welcome to redistribute it
+** under certain conditions; see LICENSE for details.
+*/
+
+#include "Engine/Application.hpp"
