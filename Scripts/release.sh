410e07c25f5a5d449c19c8c28ceadd162aa2d6bf
Mathieu Lala <mathieu.lala@epitech.eu>
2020-05-09 16:55:35 +0200

    docs: change doxyfile, simplier, cleaner (#14)
    * chore: add versioning and release automation
    
    * Release version 0.0.2
    
    * release: release version 0.0.3
    
    * docs: change doxyfile, simplier, cleaner
    
    * docs: updated doc
    
    * fix: resolve conflict

---
+++ Scripts/release.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+## Bitbucket
+#commit=${BITBUCKET_COMMIT:-$(git rev-parse HEAD)}
+
+# Gitlab
+commit=${CI_COMMIT_SHA:-$(git rev-parse HEAD)}
+
+# Define ALLOWED_RELEASE_BRANCH in VCS pipelines secret variables.
+branch=${ALLOWED_RELEASE_BRANCH:-master}
+
+if ! git branch -a --contains "${commit}" | grep -e "^[* ]*remotes/origin/${branch}\$"
+then
+  echo -e "###\n### Not on ${branch}. Only ${branch} commits can be released.\n###"
+  exit 1
+else
+  echo -e "###\n### Releasing of ${commit} on ${branch}\n###"
+fi
+
+version=$(./Scripts/version.py get)
+version_file=VERSION
+echo ${version} > ${version_file}
+
+echo "Pushing detached tag of new version"
+git add ${version_file}
+git commit -m "release: release version ${version}"
+git tag -a ${version} -m "Release version ${version} tag"
+git push origin ${version}
+
+echo "Pushing new version to ${branch}"
+git fetch origin "${branch}:${branch}" || git pull
+git checkout "${branch}"
+
+# Parameter of the script
+release_type=${1}
+./Scripts/version.py inc-${release_type}
+
+next_working_version=$(./Scripts/version.py get)
+git add ${version_file}
+git commit -m "chore: incrementing working version to ${next_working_version} after ${version} release."
+git push origin ${branch}
---
+++ Scripts/version.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import os
+import re
+
+import fire
+
+version_filepath = os.path.join('.', 'VERSION')
+version_pattern = re.compile(fr'^\d+.\d+.\d+$')
+
+def get():
+    with open(version_filepath, 'r') as version_file:
+        version_lines = version_file.readlines()
+        assert len(version_lines) == 1, 'Version file is malformed'
+        version = version_lines[0]
+        assert version_pattern.match(version), 'Version string is malformed'
+        return version
+
+def write_version_file(major: int, minor: int, patch: int):
+    version = f'{major}.{minor}.{patch}'
+    with open(version_filepath, 'w') as version_file:
+        version_file.write(version)
+
+
+def inc_patch():
+    version = get()
+    major, minor, patch = version.split('.')
+    write_version_file(major, minor, int(patch) + 1)
+
+
+def inc_minor():
+    version = get()
+    major, minor, patch = version.split('.')
+    write_version_file(major, int(minor) + 1, patch)
+
+
+def inc_major():
+    version = get()
+    major, minor, patch = version.split('.')
+    write_version_file(int(major) + 1, minor, patch)
+
+
+if __name__ == "__main__":
+    fire.Fire({
+        'get': get,
+        'inc-patch': inc_patch,
+        'inc-minor': inc_minor,
+        'inc-major': inc_major
+    })
