4cfdc3d686f133769d0c6608ef3fd475a070fc1a
Perzi-Lgs <52839257+Perzi-Lgs@users.noreply.github.com>
2020-05-12 16:06:06 +0200

    docs: add doxygen documentation in the Gui related files (#16)
    * docs: add doxygen documentation in the Gui related files
    
    * fix: fix compilation errors
    
    Compilation errors create by changing the variable name for better readability
    
    3
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/Widget/WButton.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/Widget/WButton.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/Widget/WMenuItem.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/Widget/WMenuItem.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/Window/MainMenuBar.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Update src/Engine/include/Engine/UI/AWidget.hpp
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * Apply suggestions from code review
    
    Apply typo fix
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>
    
    * fix: commit requested fix
    
    Co-authored-by: Maxime Houis <maxime.houis@epitech.eu>

--- src/Engine/include/Engine/UI/AWidget.hpp
+++ src/Engine/include/Engine/UI/AWidget.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file AWidget.hpp
+ * @author Pierre Langlois (pierre.langlois@epitech.eu)
+ * @brief 
+ * @version 0.1
+ * @date 2020-05-11
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #pragma once
 
 #include <imgui.h>
@@ -9,22 +20,70 @@ namespace usa {
 
 namespace Engine {
 
+/**
+ * @class AWidget
+ * 
+ * @brief abstract widget base class
+ * 
+ */
 class AWidget
 {
 protected:
     std::string_view m_name{};
     ImVec2 m_size{};
 public:
+    /**
+     * @brief Construct a new AWidget object
+     * 
+     * @param name The name of the widget, the name will be rendered
+     * @param size The size of the widget
+     */
     AWidget(const std::string_view &name, ImVec2 size = {}) : m_name(name), m_size(size) {}
+    
+    /**
+     * @brief Destroy the AWidget object
+     * 
+     */
     virtual ~AWidget() = default;
 
+    /**
+     * @brief Set the size of the object
+     * 
+     * @param size set the size of the widget
+     */
     auto setSize(const ImVec2 &size)        noexcept -> void { m_size = size; }
+    /**
+     * @brief Set the name of the object
+     * 
+     * @param name 
+     */
     auto setName(const std::string_view &name)   noexcept -> void { m_name = name; }
 
+    /**
+     * @brief Get the size of the object
+     * 
+     * @return const ImVec2& 
+     */
     auto getSize() -> const ImVec2 &        { return m_size; }
+
+    /**
+     * @brief Get the name of the object
+     * 
+     * @return const std::string_view& 
+     */
     auto getName() -> const std::string_view &   { return m_name; }
 
+    /**
+     * @brief render the widget
+     * 
+     */
     virtual auto render() -> void = 0;
+ 
+    /**
+     * @brief bind an action to the widget
+     * 
+     * @param std::function<void()> std::function to bind 
+     */
     virtual auto bindAction(std::function<void()>) -> void = 0;
 };
 
--- src/Engine/include/Engine/UI/UiWindow.hpp
+++ src/Engine/include/Engine/UI/UiWindow.hpp
@@ -12,6 +12,12 @@ namespace usa {
 
 namespace Engine {
 
+    /**
+     * @class UiWindow
+     * 
+     * @brief Window base class
+     * 
+     */
     class UiWindow
     {
     private:
@@ -25,31 +31,147 @@ namespace Engine {
         std::vector<std::shared_ptr<AWidget>> m_WidgetList{};
         std::vector<std::shared_ptr<UiWindow>> m_WindowList{};
     public:
+        /**
+         * @brief Construct a new Ui Window object
+         * 
+         */
         UiWindow() = default;
+
+        /**
+         * @brief Construct a new Ui Window object
+         * 
+         * @param name The name of the window, the name may be rendered
+         * @param flags The flags parameters of the window
+         */
         UiWindow(const std::string_view &name, ImGuiWindowFlags flags = 0) : m_window_flags(flags), m_name(name) {}
+
+        /**
+         * @brief Destroy the Ui Window object
+         * 
+         */
         virtual ~UiWindow() = default;
 
+        /**
+         * @brief Open the window
+         * 
+         */
         auto open()     -> void { m_isOpen = true; }
+
+        /**
+         * @brief Close the window
+         * 
+         */
         auto close()    -> void { m_isOpen = false; }
+
+        /**
+         * @brief Render the window and everything it contains
+         * 
+         */
         virtual auto render()   -> void;
-        
+
+        /**
+         * @brief Set the Window Flags
+         * 
+         * @param flags The value of the flags, different value will trigger different options
+         */
         auto setWindowFlags(ImGuiWindowFlags flags) noexcept -> void { m_window_flags = flags; }
+
+        /**
+         * @brief Set the position of the object
+         * 
+         * @param pos The new postion of the object
+         */
         auto setPosition(const ImVec2 &pos) noexcept    -> void { m_position = pos; }
+
+        /**
+         * @brief Set the size of the object
+         * 
+         * @param size The new size of the object
+         */
         auto setSize(const ImVec2 &size)    noexcept    -> void { m_size = size; }
+
+        /**
+         * @brief Set the name of the object
+         * 
+         * @param name The new name of the object
+         */
         auto setName(const std::string_view &name) noexcept  -> void { m_name = name; }
 
+        /**
+         * @brief Get the Flags object
+         * 
+         * @return ImGuiWindowFlags& 
+         */
         auto getFlags()     -> ImGuiWindowFlags &{ return m_window_flags; }
+
+        /**
+         * @brief Get the position of the object
+         * 
+         * @return const ImVec2& 
+         */
         auto getPosition()  -> const ImVec2 & { return m_position; }
+        /**
+         * @brief Get the size of the object
+         * 
+         * @return const ImVec2& 
+         */
         auto getSize()      -> const ImVec2 & { return m_size; }
+        /**
+         * @brief Get the name of the object
+         * 
+         * @return const std::string_view& 
+         */
         auto getName()      -> const std::string_view & { return m_name; }
 
-        auto addWidget(std::shared_ptr<AWidget> wid) -> void { m_WidgetList.emplace_back(wid); }
+        /**
+         * @brief Add a new widget to the window
+         * 
+         * @param widget The new widget to add 
+         */
+        auto addWidget(std::shared_ptr<AWidget> widget) -> void { m_WidgetList.emplace_back(widget); }
+        
+        /**
+         * @brief Delete a widget stored in the window
+         * 
+         * @param name The name of the widget to delete
+         * @return true Return true if a widget was delete
+         * @return false Return false if no widget was found
+         */
         auto deleteWidget(const std::string_view &name)  -> bool;
-        auto getWidget(const std::string_view &name)     -> std::optional<std::shared_ptr<AWidget>>;
+        
+        /**
+         * @brief Get the Widget object
+         * 
+         * @param name The name of the widget to get
+         * @return std::shared_ptr<AWidget> Return a widget smart pointer if found
+         * @return nullptr Return nullptr if the widget was not found
+         */
+        auto getWidget(const std::string_view &name)     -> std::shared_ptr<AWidget>;
 
-        auto addWindow(std::shared_ptr<UiWindow> wid) -> void { m_WindowList.emplace_back(wid); }
+        /**
+         * @brief Add a new sub Window in the current window
+         * 
+         * @param win The new window to add 
+         */
+        auto addWindow(std::shared_ptr<UiWindow> win) -> void { m_WindowList.emplace_back(win); }
+        
+        /**
+         * @brief Delete a sub-window stored in the window
+         * 
+         * @param name the name of the sub-window to delete
+         * @return true return true if a window was deleted
+         * @return false return false otherwise
+         */
         auto deleteWindow(const std::string_view &name) -> bool;
-        auto getWindow(const std::string_view &name) -> std::optional<std::shared_ptr<UiWindow>>;
+        
+        /**
+         * @brief Get the Window object
+         * 
+         * @param name The name of the window object to get
+         * @return std::shared_ptr<UiWindow> return a UiWindow smart pointer
+         * @return nullptr return nullptr otherwise
+         */
+        auto getWindow(const std::string_view &name) -> std::shared_ptr<UiWindow>;
     };
 }
 
