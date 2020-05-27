e44607cea9b7e6d364b785a8bae67e6229680506
Maxime Houis <maxime.houis@epitech.eu>
2020-05-27 10:36:46 +0200

    docs: add engine documentation

--- src/Engine/include/Engine/Settings/Settings.hpp
+++ src/Engine/include/Engine/Settings/Settings.hpp
@@ -13,7 +13,13 @@
 
 namespace Engine {
 
+/**
+ * @brief The Settings class represents the application's settings. It is serializable/deserializable in JSON format.
+ */
 struct Settings {
+    /**
+     * @brief Where the config file is located.
+     */
     static constexpr auto CONFIG_PATH = "Resources/config.json";
 
     // Default values that can be overwritten in the config file
@@ -22,10 +28,31 @@ struct Settings {
     std::uint32_t fps_limit{0};
     bool fullscreen{false};
 
+    /**
+     * @brief Loads settings from a JSON file.
+     * @param path Where to load the config from. Defaults to CONFIG_PATH.
+     * @return Settings class.
+     */
     static auto fromFile(const std::string_view &path = CONFIG_PATH) -> Settings;
+
+    /**
+     * @brief Saves current application settings.
+     * @param path Where to save the config to. Defaults to CONFIG_PATH.
+     */
     auto save(const std::string_view &path = CONFIG_PATH) const -> void;
 };
 
+/**
+ * @brief Used by nlohmann::json serialization.
+ * @param json JSON object.
+ * @param settings Settings object.
+ */
 void to_json(Json &json, const Settings &settings);
+
+/**
+ * @brief Used by nlohmann::json deserialization.
+ * @param json JSON object.
+ * @param settings Settings object.
+ */
 void from_json(const Json &json, Settings &settings);
 } // namespace Engine
